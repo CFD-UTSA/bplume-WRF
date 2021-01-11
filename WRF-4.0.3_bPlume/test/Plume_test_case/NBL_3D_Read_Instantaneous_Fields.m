56520de3a4d0977344042fc115aeb0d0e8d22141
sudheer-wrf <77242507+sudheer-wrf@users.noreply.github.com>
2021-01-10 22:40:05 -0600

    Update NBL_3D_Read_Instantaneous_Fields.m

--- WRF-4.0.3_bPlume/test/Plume_test_case/NBL_3D_Read_Instantaneous_Fields.m
+++ WRF-4.0.3_bPlume/test/Plume_test_case/NBL_3D_Read_Instantaneous_Fields.m
@@ -1,12 +1,24 @@
-%%
+%% 
+% Clear workspace and memory
 clearvars;
 clc;
 
 %%
-Files_directory = './';
+% Prior to running this script, move all the high-frequency files to a folder
 
+Files_directory = './High-Frequency/';
+
+% get all the files in the directory
 fileslist = dir(Files_directory);
+
+% get number of files
 nFiles    = size(fileslist,1);
+
+% Check the below start and end indices
+% On Linux OS of TACC Stampede, first two entries of filelist are dummies . and ..
+% To confirm, run line 8 and see the fileslist array. 
+% If the high-frequency files start at 1st entry, change the below line to 
+% for iFile = 1:nFiles
 
 for iFile = 3:nFiles
     filename  = fileslist(iFile).name;
@@ -25,7 +37,7 @@ for iFile = 3:nFiles
         k = k + 1;
         %     fprintf('Processed - %s\n',num2str((kk/totalRows)*100));
     end
-    saveFile = strcat('./',filename,'.mat');
+    saveFile = strcat('./High-Frequency/',filename,'.mat');
     save(saveFile,'data');
     clear data     
 end
